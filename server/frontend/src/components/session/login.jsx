bb4d87203b6cfcc905ec59b9e1e7d2a3bdacd4cf
Julie Tran <kimtran767@gmail.com>
2020-07-09 10:39:59 -0700

    useSelector to pull out state management error for session

--- server/frontend/src/components/session/login.jsx
+++ server/frontend/src/components/session/login.jsx
@@ -1,13 +1,18 @@
 // effect is like react life cycle meethod
 import React, { useState, useEffect } from 'react';
 
-import { useDispatch, useSelctor } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { login, clearErrors } from '../../actions/session_actions';
 
 const LoginForm = () => {
     const dispatch = useDispatch();
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
+    const errors = useSelector((state) => Object.values(state.errors.session));
+
+      useEffect(() => {
+        if (errors.length) dispatch(clearErrors());
+      }, []);
 
     const updateEmail = e => setEmail(e.target.value);
     const updatePassword = e => setPassword(e.target.value);
@@ -23,21 +28,26 @@ const LoginForm = () => {
     }
 
     return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input value={email} onChange={updateEmail} />
-        </label>
-        <label>
-          Password
-          <input
-            value={password}
-            onChange={updatePassword}
-            type='password'
-          />
-        </label>
-        <input type='submit' value='Log In' />
-      </form>
+      <div>
+        <div>
+          {errors.map((err, i) => (
+            <div key={i}>
+              {err}
+            </div>
+          ))}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email
+            <input value={email} onChange={updateEmail} />
+          </label>
+          <label>
+            Password
+            <input value={password} onChange={updatePassword} type='password' />
+          </label>
+          <input type='submit' value='Log In' />
+        </form>
+      </div>
     );
 };
 

84b18befdfabd9df821f5beff4ac80750c61a100
Julie Tran <kimtran767@gmail.com>
2020-07-15 08:50:52 -0700

    completed all user register validation

--- server/validation/register.js
+++ server/validation/register.js
@@ -8,6 +8,29 @@ module.exports = function validateRegisteerInput(data) {
     data.companyName = validText(data.companyName) ? data.companyName : '';
     data.password = validText(data.password) ? data.password : '';
 
-    if (!Validator.isEmail(data.email))
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Please provide a valid email.'
+    };
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email is required.'
+    };
+
+    if (Validator.isEmpty(data.companyName)) {
+        errors.companyName = 'Company name is required.'
+    };
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password is required.'
+    };
+
+    if (Validator.isLength(data.password, { min: 8, max: 30})) {
+        errors.password = 'Password must be between 6 and 30 characters.'
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    };
     
 };

2b80a8a0f5104c3f031971a07cb62c5364e52bf7
Julie Tran <kimtran767@gmail.com>
2020-10-06 10:29:18 -0700

    created a component for create customer

---
+++ frontend/src/components/customers/add_customer.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+const AddCustomer = ({currentUserId}) => {
+    const dispatch = useDispatch();
+};
+
+
+export default AddCustomer;

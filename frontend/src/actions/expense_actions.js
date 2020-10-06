cd269c2ad4915c3369660355a178a0fe6a998479
Julie Tran <kimtran767@gmail.com>
2020-10-06 11:01:27 -0700

    expense action done on crud method

---
+++ frontend/src/actions/expense_actions.js
@@ -0,0 +1,38 @@
+import * as ExpenseUtil from '../utils/expenses_util';
+
+export const RECIEVE_ALL_EXPENSES = 'RECIEVE_ALL_EXPENSES';
+export const RECEIVE_EXPENSE = 'RECEIVE_EXPENSE';
+export const REMOVE_EXPENSE = 'REMOVE_EXPENSE';
+
+const receiveAllExpenses = expenses => {
+    return {
+        type: RECIEVE_ALL_EXPENSES,
+        expenses
+    }
+};
+
+const receiveExpense = expense => {
+    return {
+        type: RECEIVE_EXPENSE,
+        expense
+    }
+};
+
+const removeExpense = expenseId => {
+    return {
+        type: REMOVE_EXPENSE,
+        expenseId
+    }
+};
+
+export const createExpense = data => dispatch => 
+    ExpenseUtil.createExpense(data)
+        .then(expense => dispatch(receiveExpense(expense)));
+
+export const updateExpense = data => dispatch => 
+    ExpenseUtil.updateExpense(data)
+        .then(expense => dispatch(receiveExpense(expense)));
+
+export const deleteExpense = data => dispatch => 
+    ExpenseUtil.deleteExpense(data)
+        .then(() => dispatch(removeExpense(data)));
